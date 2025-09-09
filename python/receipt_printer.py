4015ec034bc0ed0416ad137a5731364bc7d170ac
Merit Ihab <mihab@envasetechnologies.com>
2025-09-10 02:06:32 +0300

    FEATURE: Add feature to print html receipt and add its unit tests

--- python/receipt_printer.py
+++ python/receipt_printer.py
@@ -8,38 +8,82 @@ class ReceiptPrinter:
     def print_receipt(self, receipt):
         lines = []
         for item in receipt.items:
-            lines.append(self.print_receipt_item(item))
+            lines.append(self._print_receipt_item(item))
         for discount in receipt.discounts:
-            lines.append(self.print_discount(discount))
-        lines.append(self.present_total(receipt))
+            lines.append(self._print_discount(discount))
+        lines.append(self._present_total(receipt))
         return "".join(str(line) for line in lines)
   
-    
-    def print_receipt_item(self, item):
-        total_price_printed = self.print_price(item.total_price)
+    def _print_receipt_item(self, item):
+        total_price_printed = self._print_price(item.total_price)
         name = item.product.name
-        line = self.format_line_with_whitespace(name, total_price_printed)
+        line = self._format_line_with_whitespace(name, total_price_printed)
         if item.quantity != 1:
-            line += f"  {self.print_price(item.price)} * {self.print_quantity(item)}\n"
+            line += f"  {self._print_price(item.price)} * {self._print_quantity(item)}\n"
         return line
 
-    def format_line_with_whitespace(self, name, value):
+    def _format_line_with_whitespace(self, name, value):
         whitespace = self.columns - len(name) - len(value)
         return f"{name}{' ' * whitespace}{value}\n"
 
-    def print_price(self, price):
+    def _print_price(self, price):
         return f"{price:.2f}"
 
-    def print_quantity(self, item):
+    def _print_quantity(self, item):
         if ProductUnit.EACH == item.product.unit:
             return str(item.quantity)
         return f"{item.quantity:.3f}"
 
-    def print_discount(self, discount):
+    def _print_discount(self, discount):
         name = f"{discount.description} ({discount.product.name})"
-        value = self.print_price(discount.discount_amount)
-        return self.format_line_with_whitespace(name, value)
+        value = self._print_price(discount.discount_amount)
+        return self._format_line_with_whitespace(name, value)
 
-    def present_total(self, receipt):
-        value = self.print_price(receipt.total_price())
-        return f"\n{self.format_line_with_whitespace('Total: ', value)}"
+    def _present_total(self, receipt):
+        value = self._print_price(receipt.total_price())
+        return f"\n{self._format_line_with_whitespace('Total: ', value)}"
+
+
+class HtmlReceiptPrinter:
+    def __init__(self, columns=40):
+        self.columns = columns
+
+    def print_receipt(self, receipt):
+        items = "".join(self._print_receipt_item(item) for item in receipt.items)
+        discounts = "".join(self._print_discount(d) for d in receipt.discounts)
+        total = self._present_total(receipt)
+        return self._html_receipt_format(items, discounts, total)
+
+    def _print_receipt_item(self, item):
+        line = f"<tr><td>{item.product.name}</td><td>{item.total_price:.2f}</td></tr>"
+        if item.quantity != 1:
+            line = f"<tr><td>{item.product.name}<br>{item.price:.2f} * {self._print_quantity(item)}</td><td>{item.total_price:.2f}</td></tr>"
+        return line
+    
+    def _print_quantity(self, item):
+        if ProductUnit.EACH == item.product.unit:
+            return str(item.quantity)
+        return f"{item.quantity:.3f}"
+
+    def _print_discount(self, discount):
+        return f"<tr><td><strong>Discount</strong></td><td>{discount.description}</td> <td>({discount.product.name}):</td> <td>{discount.discount_amount:.2f}</td></tr>"
+
+    def _present_total(self, receipt):
+        return f"<tr><td><strong>Total:</strong></td> <td><strong>{receipt.total_price():.2f}</strong></td></tr>"
+
+    def _html_receipt_format(self, items, discounts, total):
+        return f"""
+            <!doctype html>
+            <html lang=\"en\">
+                <head><meta charset=\"utf-8\"><title>Receipt</title></head>
+                <body>
+                    <h1>Receipt</h1>
+                    <table cellspacing="15">
+                        {items}
+                        {discounts}
+                        <tr></tr><tr></tr>
+                        {total}
+                    </table>
+                </body>
+            </html>
+        """.strip()
